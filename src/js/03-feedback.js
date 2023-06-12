ba58dbb486391c1481c6074a55412e5b221c0100
Leo041085 <131529700+Leo041085@users.noreply.github.com>
2023-06-13 00:07:03 +0300

    update hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+
+const savedState = throttle(() => {
+  const email = form.email.value;
+  const message = form.message.value;
+    const state = { email, message };
+    
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}, 500);
+
+const initialState = localStorage.getItem('feedback-form-state');
+
+if (initialState) {
+  const { email, message } = JSON.parse(initialState);
+  form.email.value = email;
+  form.message.value = message;
+};
+
+form.addEventListener('input', savedState);
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+    
+  const email = form.email.value;
+  const message = form.message.value;
+  const state = { email, message };
+  localStorage.removeItem('feedback-form-state');
+  form.email.value = '';
+  form.message.value = '';
+  console.log(state);
+});
